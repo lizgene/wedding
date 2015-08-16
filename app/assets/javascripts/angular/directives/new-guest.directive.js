e6939f1bf682607a3469b6225a4f232831c07754
Liz Hubertz <liz.hubertz@gmail.com>
2015-08-16 14:01:02 +0700

    working new guest directive for admin and rsvp form

--- app/assets/javascripts/angular/directives/new-guest.directive.js
+++ app/assets/javascripts/angular/directives/new-guest.directive.js
@@ -1,19 +1,23 @@
-angular.module('app.directives').directive('newGuest', ['Guest', '$stateParams', function(Guest, $stateParams){
+angular.module('app.directives').directive('newGuest', ['Guest', function(Guest){
   return {
     restrict: 'E',
     transclude: true,
     scope: {
-      guests: '=guests'
+      guests: '=guests',
+      invitation: '=invitation',
+      approved: '@'
     },
     templateUrl: 'directives/new-guest.html',
     bindToController: true,
     controllerAs: 'newGuestCtrl',
     controller: function() {
       var vm = this
-
-      vm.newGuest = { "invitation_id": $stateParams.id };
+      vm.newGuest = { "attending": true, "approved": vm.approved };
 
       vm.addGuest = function(newGuest){
+        //need to populate invitation_id after the page has finished any get requests
+        newGuest.invitation_id = vm.invitation.id
+
         Guest.create({guest: newGuest}, function(response) {
           vm.guests.push(response.guest);
         });
